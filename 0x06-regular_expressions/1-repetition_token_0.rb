719790269c1d040e527321787213f5cd4feb4c6f
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2024-05-01 22:16:32 +0300

    Update 1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit
-end
-
-# The provided string
-test_string = ARGV[0]
-
-# Regular expression pattern to match 'hbt+n'
-pattern = /hbt+n/
-
-# Check if the test string matches the pattern
-if test_string.match?(pattern)
-  puts "'#{test_string}' matches the pattern!"
-else
-  puts "'#{test_string}' does not match the pattern."
-end
+puts ARGV[0].match(/hbt{2,5}n/)
